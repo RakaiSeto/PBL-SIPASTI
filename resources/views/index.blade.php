1974a7350a591bba0282cd19eb3f2e084cdc1e68
Rakai Seto <mamangrakai@gmail.com>
2025-05-05 13:45:19 +0700

    feat(index): update favicon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Sistem Pelaporan - Landing Page</title>
     <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="shortcut icon" href="{{asset('assets/image/LOGO.svg')}}" type="image/x-icon">
     <style>
       :root {
         --primary: #1652b7;
