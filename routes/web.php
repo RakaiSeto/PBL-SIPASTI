4b1edb9d2929c411fb1acc68c8600a5120d6c23d
Agung Fradiansyah <agungfradiansyah01@gmail.com>
2025-05-12 11:42:53 +0700

    grouping route

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -29,57 +18,64 @@ Route::get('/register', function () {
     return view('auth.register');
 });
 
-Route::get('/admin/dashboard', function () {
-    return view('admin.index');
+// Group untuk Admin
+Route::prefix('admin')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    });
+
+    Route::get('/admin', function () {
+        return view('admin.mahasiswa.index');
+    });
+
+    Route::get('/datapengguna', function () {
+        return view('admin.datapengguna.index');
+    });
+
+    Route::get('/fasilitas', function () {
+        return view('admin.fasilitas.index');
+    });
+
+    Route::get('/ruangan', function () {
+        return view('admin.ruangan.index');
+    });
 });
 
+// Route tambahan menuju admin (duplikat dashboard)
 Route::get('/hai', function () {
     return view('admin.index');
 });
 
+// Group untuk Civitas
+Route::prefix('civitas')->group(function () {
+    Route::get('/', function () {
+        return view('civitas.index');
+    });
 
+    Route::get('/laporkan', function () {
+        return view('civitas.laporkan');
+    });
 
-Route::get('/admin/admin', function () {
-    return view('admin.mahasiswa.index');
+    Route::get('/status', function () {
+        return view('civitas.status');
+    });
+
+    Route::get('/rating', function () {
+        return view('civitas.rating');
+    });
 });
 
+// Group untuk Teknisi
+Route::prefix('teknisi')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('teknisi.index');
+    });
 
-Route::get('/admin/datapengguna', function () {
-    return view('admin.datapengguna.index');
-});
+    Route::get('/tugas', function () {
+        return view('teknisi.tugas');
+    });
 
-Route::get('/admin/fasilitas', function () {
-    return view('admin.fasilitas.index');
-});
-
-Route::get('/admin/ruangan', function () {
-    return view('admin.ruangan.index');
-});
-
-Route::get('/civitas', function () {
-    return view('civitas.index');
-});
-
-Route::get('/civitas/laporkan', function () {
-    return view('civitas.laporkan');
-});
-
-Route::get('/civitas/status', function () {
-    return view('civitas.status');
-});
-
-Route::get('/civitas/rating', function () {
-    return view('civitas.rating');
-});
-
-Route::get('/teknisi/dashboard', function () {
-    return view('teknisi.index');
-});
-
-Route::get('/teknisi/tugas', function () {
-    return view('teknisi.tugas');
-});
-
-Route::get('/teknisi/riwayat', function () {
-    return view('teknisi.riwayat');
+    Route::get('/riwayat', function () {
+        return view('teknisi.riwayat');
+    });
 });
